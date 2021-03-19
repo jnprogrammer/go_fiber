3945db0c700ddf7d2f949f79b53140ea2c4177f8
jnprogrammer <joshuanparker@gmail.com>
2021-03-18 23:35:24 -0400

    finshied api to allow custom POSTs

--- book/book.go
+++ book/book.go
@@ -22,13 +22,35 @@ func GetBooks(c *fiber.Ctx) {
 }
 
 func GetBook(c *fiber.Ctx) {
-	c.Send("A Book")
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+	db.Find(&book, id)
+	c.JSON(book)
+
 }
 
 func NewBook(c *fiber.Ctx) {
-	c.Send("Add Book")
+	db := database.DBConn
+	book := new(Book)
+	if err := c.BodyParser(book); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&book)
+	c.JSON(book)
+
 }
 
 func DeleteBook(c *fiber.Ctx) {
-	c.Send("Delete Book")
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+
+	db.Find(&book, id)
+	if book.Title == "" {
+		c.Status(500).Send("No book found with given ID")
+	}
+	db.Delete(&book)
+	c.Send("Big Brother was watched you")
 }
