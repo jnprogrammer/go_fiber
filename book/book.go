c8f483d62b2e0e467c77cd9786e6dc4c60b24422
jnprogrammer <joshuanparker@gmail.com>
2021-03-18 23:05:14 -0400

    simple DB migration sucessful

--- book/book.go
+++ book/book.go
@@ -1,9 +1,24 @@
 package book
 
-import "github.com/gofiber/fiber"
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/jnprogrammer/go_fiber/database"
+)
+
+type Book struct {
+	gorm.Model
+	Title  string //'json:"title"'
+	Author string // 'json:"author"'
+	Rating int    //'json:"rating"'
+
+}
 
 func GetBooks(c *fiber.Ctx) {
-	c.Send("All Books")
+	db := database.DBConn
+	var books []Book
+	db.Find(&books)
+	c.JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) {
@@ -14,6 +29,6 @@ func NewBook(c *fiber.Ctx) {
 	c.Send("Add Book")
 }
 
-func Deletebook(c *fiber.Ctx) {
+func DeleteBook(c *fiber.Ctx) {
 	c.Send("Delete Book")
 }
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/jnprogrammer/go_fiber/book"
+	"github.com/jnprogrammer/go_fiber/database"
 )
 
 func helloWorld(c *fiber.Ctx) {
@@ -13,11 +17,25 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/book", book.GetBooks)
 	app.Get("/api/v1/book/:id", book.GetBook)
 	app.Post("/api/v1/book", book.NewBook)
-	app.Delete("/api/v1/book/:id", book.Deletebook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failled DB connection")
+	}
+	fmt.Println("Database connection opened")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("DB migrated")
 }
 
 func main() {
 	app := fiber.New()
+	initDatabase()
+	defer database.DBConn.Close()
 
 	setupRoutes(app)
 
